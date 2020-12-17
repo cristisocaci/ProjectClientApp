86375dde48022dec587827001780d23070d32d1f
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-17 17:13:15 +0200

    Initial commit

---
+++ presentation-app/src/app/page404/page404.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page404',
+  templateUrl: './page404.component.html',
+  styleUrls: ['./page404.component.css']
+})
+export class Page404Component implements OnInit {
+  
+  logo = sessionStorage.getItem("logo");
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ presentation-app/src/app/shared/info.ts
@@ -0,0 +1,8 @@
+export class Info {
+    infoId: string;
+    type: string;
+    content: string;
+    position: number;
+    projectId: number;
+    additionalData: string;
+}
