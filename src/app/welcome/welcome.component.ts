23a503cd073067b3910b383576b78f61f542bd0b
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-01-20 22:49:05 +0200

    call backend in welcome page to start up the app faster on heroku

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Identity } from '../shared/identity';
+import { ProjectsService } from '../shared/projects.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,11 +12,17 @@ export class WelcomeComponent implements OnInit {
   identity: Identity;
   logo = sessionStorage.getItem("logo");
 
-  constructor( identity: Identity ) { 
+  constructor( identity: Identity,
+              private projectService: ProjectsService, ) { 
     this.identity = identity;
   }
 
   ngOnInit(): void {
+    this.projectService.loadProjects("0").subscribe(success =>{ 
+      if (success) {
+        console.log('ok')
+      }
+    })
   }
   
 }
